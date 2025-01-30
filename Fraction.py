bcdb73e99539cde732fd0da7cf693da0cbdaade0
Sticity <119797972+Sticity@users.noreply.github.com>
2025-01-30 13:35:09 +0800

    refactored the code logic for every function

--- Fraction.py
+++ Fraction.py
@@ -6,14 +6,18 @@ class Fraction(object):
 
     def gcd(a, b):
         if (b == 0):
-            return 0
+            return a
         return (gcd(b, a % b))
 
     def get_numerator(self):
-        return '' + self.numerator
+        lowest = self.numerator / self.gcd(self.numerator, self.denominator)
+        
+        return '' + lowest
 
     def get_denominator(self):
+        lowest = self.denominator / self.gcd(self.numerator, self.denominator)
+        
         return '' + self.denominator
 
     def get_fraction(self):
-        
+        return 'f{self.numerator}/{self.denominator}'
