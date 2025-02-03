af8d7ebcf06820f50242c55cb7fd3477ffc82807
cachooo <70294001+cachooo@users.noreply.github.com>
2025-02-03 18:22:14 +0800

    feat: implement working logic for Fraction class

--- Fraction.py
+++ Fraction.py
@@ -1,15 +1,40 @@
 class Fraction(object):
 
     def __init__(self, numerator=0, denominator=1):
-        if self.is_int(numerator) and self.is_int(denominator):
-            self.numerator = int(numerator)
-            self.denominator = int(denominator)
+        self.numerator = 0 
+        self.denominator = 1
 
-        if denominator == 0:
+        try:
+            if isinstance(numerator, str):
+                numerator = numerator.strip()
+                if '/' in numerator:
+                    num, denom = numerator.split('/')
+                    numerator = int(num)
+                    denominator = int(denom)
+                else:
+                    raise ValueError
+            elif isinstance(numerator, float):
+                raise ValueError
+            
+            if self.is_int(numerator) and self.is_int(denominator):
+                self.numerator = int(numerator)
+                self.denominator = int(denominator)
+            else:
+                raise ValueError
+        
+        except (ValueError, TypeError):
+            self.numerator = 0
+            self.denominator = 1
+        
+        if self.denominator == 0: 
             raise ZeroDivisionError
+    
+        if self.numerator != 0 and self.denominator != 0:
+            self.gcd = Fraction.gcd(self.numerator, self.denominator)
 
-        self.numerator = 0
-        self.denominator = 0
+        else:
+            self.gcd = 1  
+
 
     @staticmethod
     def is_int(string):
@@ -27,21 +52,29 @@ class Fraction(object):
         return a
 
     def get_numerator(self):
-        lowest = self.numerator / self.gcd(self.numerator, self.denominator)
         
-        return '' + lowest
+        if self.gcd == 0: 
+            return 0 
+
+        numerator = self.numerator // self.gcd
+        
+        return str(numerator)
 
     def get_denominator(self):
-        lowest = self.denominator / self.gcd(self.numerator, self.denominator)
+        if self.gcd == 0: 
+            return 0 
+
+        denominator = self.denominator // self.gcd
         
-        return '' + lowest
+        return str(denominator)
 
     def get_fraction(self):
         numerator = self.get_numerator()
         denominator = self.get_denominator()
         
-        if numerator == 0:
+        if numerator == "0":
             return "0"
-        if denominator == 1:
-            return str(numerator)
+        if denominator == "1":
+            return numerator
+        
         return f"{numerator}/{denominator}"
